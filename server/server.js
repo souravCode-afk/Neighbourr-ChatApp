deecacc1c11eca9ddfa8161593bbe825b08a2402
Sourav Sharma <souravsharma20222324@gmail.com>
2026-06-04 17:04:41 +0530

    fix: update backend deployment

--- server/server.js
+++ server/server.js
@@ -41,14 +41,19 @@ io.on("connection",(socket)=>{
 // Middleware setup
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: "https://neighbourr-frontend.vercel.app",
+    credentials: true
+}));
 
 // Routes setup
-app.use("/api/status",(req,res)=>res.send("Server is live"));
+app.get("/api/status",(req,res)=>res.send("Server is live"));
 app.use("/api/auth",userRouter)
 app.use("/api/messages",messageRouter)
 //Connect to MongoDB
 await connectDB();
 
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>console.log("Server is running on PORT:" + PORT));
+
 // Export server for vercel
-export default app;

71ae848e0bc698e868d442a4d028eb823f3058a1
Sourav Sharma <souravsharma20222324@gmail.com>
2026-06-09 19:08:18 +0530

    chat room feature updated

--- server/server.js
+++ server/server.js
@@ -82,7 +82,10 @@ io.on("connection",(socket)=>{
 // Middleware setup
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://neighbourr-frontend.vercel.app',
+    credentials: true
+}));
 
 // Routes setup
 app.get("/api/status",(req,res)=>res.send("Server is live"));
